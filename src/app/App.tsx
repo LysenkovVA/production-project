8cd36dd480065fd3afc872e5b76f8d3a58806b80
Raudbrand <raudbrand@yandex.ru>
2023-02-05 22:34:59 +0300

    lesson 11

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,7 @@ import {Route, Routes, Link} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -14,12 +13,7 @@ const App = () => {
             <button onClick={toggleTheme}>Theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
