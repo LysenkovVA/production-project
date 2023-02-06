6cbccc39a5b14fd575642c4ecd2149bad0c61459
Raudbrand <raudbrand@yandex.ru>
2023-02-06 22:48:23 +0300

    lesson 12

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,10 +1,10 @@
 type Mods = Record<string, boolean | string>;
 
 // Функция для удобного комбинирования классов
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
             // Оставляем элементы со значением true
             .filter(([className, value]) => Boolean(value))
