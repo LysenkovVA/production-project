3ff382aeb8fd42550145222092495fbaaeac06e7
Raudbrand <raudbrand@yandex.ru>
2023-03-07 13:04:23 +0300

    add test for login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginLoading/getLoginIsLoading.test.ts
@@ -0,0 +1,18 @@
+import {getLoginIsLoading} from "./getLoginIsLoading";
+import {DeepPartial} from "@reduxjs/toolkit";
+import {StateSchema} from "app/providers/StoreProvider";
+
+describe("getLoginIsLoading.test", () => {
+    test("should return true", () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                isLoading: true
+            }
+        };
+        expect(getLoginIsLoading(state as StateSchema)).toEqual(true);
+    });
+    test("should return false", () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginIsLoading(state as StateSchema)).toEqual(false);
+    });
+});
