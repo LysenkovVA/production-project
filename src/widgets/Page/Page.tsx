ce5709ac29dc0fce4ba5b145e9555cb800b66e43
Raudbrand <raudbrand@yandex.ru>
2023-04-14 15:53:10 +0300

    semantics

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -50,7 +50,7 @@ export const Page = memo((props: PageProps) => {
     }, 500);
 
     return (
-        <section
+        <main
             ref={wrapperRef}
             className={classNames(cls.Page, {}, [className])}
             onScroll={onScroll}
@@ -63,6 +63,6 @@ export const Page = memo((props: PageProps) => {
                     ref={triggerRef}
                 />
                 : null}
-        </section>
+        </main>
     );
 });
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -30,7 +30,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
     }), [collapsed, sidebarItemsList]);
 
     return (
-        <menu
+        <aside
             data-testid="sidebar"
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
@@ -46,6 +46,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
                 {collapsed ? ">" : "<"}
             </Button>
             <VStack
+                role={"navigation"}
                 gap={"8"}
                 className={cls.items}
             >
@@ -58,6 +59,6 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
                     className={cls.lang}
                 />
             </div>
-        </menu>
+        </aside>
     );
 });
