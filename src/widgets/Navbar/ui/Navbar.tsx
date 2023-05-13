cbe5a5755a7bf227ac56de4d1e58f308f636d94e
Raudbrand <raudbrand@yandex.ru>
2023-05-13 11:55:27 +0300

    refactoring router

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -6,12 +6,12 @@ import {Button, ButtonTheme} from "@/shared/ui/Button/Button";
 import {LoginModal} from "@/features/AuthByUsername";
 import {Text, TextTheme} from "@/shared/ui/Text/Text";
 import {AppLink, AppLinkTheme} from "@/shared/ui/AppLink/AppLink";
-import {RoutePath} from "@/shared/config/routeConfig/routeConfig";
 import {HStack} from "@/shared/ui/Stack";
 import {NotificationButton} from "@/features/notificationButton";
 import {AvatarDropdown} from "@/features/avatarDropdown";
 import {useSelector} from "react-redux";
 import {getUserAuthData} from "@/entities/User";
+import {RoutePath} from "@/shared/const/router";
 
 interface NavbarProps {
     className?: string;
