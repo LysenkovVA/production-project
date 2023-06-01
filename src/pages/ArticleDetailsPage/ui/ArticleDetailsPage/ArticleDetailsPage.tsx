54963f495d4b1c07c39012c08d0c153850c24713
Raudbrand <raudbrand@yandex.ru>
2023-06-01 21:13:16 +0300

    add feature flags concept

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,26 +1,34 @@
-import {classNames} from "@/shared/lib/classNames/classNames";
-import {memo} from "react";
-import {ArticleDetails} from "@/entities/Article";
-import {useParams} from "react-router-dom";
-import {DynamicModuleLoader, ReducersList} from "@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
-import {Page} from "@/widgets/Page";
-import {articleDetailsPageReducer} from "../../model/slice";
-import {ArticleDetailsPageHeader} from "../ArticleDetailsPageHeader/ArticleDetailsPageHeader";
-import {VStack} from "@/shared/ui/Stack";
-import {ArticleRecommendationsList} from "@/features/articleRecommendationsList";
-import {ArticleDetailsComments} from "../ArticleDetailsComments/ArticleDetailsComments";
-import {ArticleRating} from "@/features/articleRating";
+import { classNames } from "@/shared/lib/classNames/classNames";
+import { memo } from "react";
+import { ArticleDetails } from "@/entities/Article";
+import { useParams } from "react-router-dom";
+import {
+    DynamicModuleLoader,
+    ReducersList,
+} from "@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
+import { Page } from "@/widgets/Page";
+import { articleDetailsPageReducer } from "../../model/slice";
+import { ArticleDetailsPageHeader } from "../ArticleDetailsPageHeader/ArticleDetailsPageHeader";
+import { VStack } from "@/shared/ui/Stack";
+import { ArticleRecommendationsList } from "@/features/articleRecommendationsList";
+import { ArticleDetailsComments } from "../ArticleDetailsComments/ArticleDetailsComments";
+import { ArticleRating } from "@/features/articleRating";
+import { getFeatureFlag } from "@/shared/lib/features";
+import { Counter } from "@/entities/Counter";
 
 interface ArticleDetailsPageProps {
     className?: string;
 }
 
 const reducers: ReducersList = {
-    articleDetailsPage: articleDetailsPageReducer
+    articleDetailsPage: articleDetailsPageReducer,
 };
 
-const ArticleDetailsPage = ({className}: ArticleDetailsPageProps) => {
-    const {id} = useParams<{id: string}>();
+const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
+    const { id } = useParams<{ id: string }>();
+
+    const isArticleRatingEnabled = getFeatureFlag("isArticleRatingEnabled");
+    const isCounterEnabled = getFeatureFlag("isCounterEnabled");
 
     if (!id) {
         return null;
@@ -32,13 +40,11 @@ const ArticleDetailsPage = ({className}: ArticleDetailsPageProps) => {
                 className={classNames("", {}, [className])}
                 data-testid={"ArticleDetailsPage"}
             >
-                <VStack
-                    gap={"16"}
-                    max
-                >
+                <VStack gap={"16"} max>
                     <ArticleDetailsPageHeader />
-                    <ArticleDetails id={id}/>
-                    <ArticleRating articleId={id}/>
+                    <ArticleDetails id={id} />
+                    {isArticleRatingEnabled && <ArticleRating articleId={id} />}
+                    {isCounterEnabled && <Counter />}
                     <ArticleRecommendationsList />
                     <ArticleDetailsComments id={id} />
                 </VStack>
